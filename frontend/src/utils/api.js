b93e8bffd35c2e944c4e5f56602715c8712e182e
arpitpracto <arpit.hajare@practo.com>
2025-02-20 10:22:39 +0530

    Delete frontend/src directory

--- frontend/src/utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-    baseURL: "http://localhost:8080",
-    headers: {
-        "Content-Type": "application/json",
-      },
-    // Adjust based on your backend
-});
